90fbaddc268602390262be905ff4cab7dce9d204
Henrik Helmø Larsen <hhl@asseco.dk>
2020-12-17 10:58:35 +0100

    Remove Number_of_Originals from TransportDocument

--- datamodel/initdb.d/03_dcsa_im_v3_0.sql
+++ datamodel/initdb.d/03_dcsa_im_v3_0.sql
@@ -78,7 +78,6 @@ CREATE TABLE dcsa_ebl_v1_0.transport_document (
 	onboard_date date NULL,
 	received_for_shipment_date date NULL,
 	document_reference_number varchar(20) NULL,
-	number_of_originals integer NULL,
 	terms_and_conditions text NULL,
 	issuer uuid NULL,	-- name of the carrier who issues the BL
 	shipping_instruction_id UUID NULL,
