eada156f934b25d90e75f01343d6c26169c12b76
Niels Thykier <nt@asseco.dk>
2020-12-17 10:52:36 +0100

    Use transactions plus ON_ERROR_STOP in SQL
    Greatly simplifies the debugging experience if a script fails.
    
    Signed-off-by: Niels Thykier <nt@asseco.dk>

--- datamodel/initdb.d/02_create_database.sql
+++ datamodel/initdb.d/02_create_database.sql
@@ -1,11 +1,16 @@
 -- Database setup script
 -- Needs to be executed by user postgres or equivalent
+\set ON_ERROR_STOP true
 
+-- Cannot be done in a transaction
 DROP DATABASE IF EXISTS dcsa_openapi;
 CREATE DATABASE dcsa_openapi OWNER dcsa_db_owner;
 \connect dcsa_openapi
+
+BEGIN;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp"; -- Used to generate UUIDs
 CREATE SCHEMA IF NOT EXISTS dcsa_ebl_v1_0;
 GRANT ALL PRIVILEGES ON DATABASE dcsa_openapi TO dcsa_db_owner;
 GRANT ALL PRIVILEGES ON SCHEMA dcsa_ebl_v1_0 TO dcsa_db_owner;
 ALTER DEFAULT PRIVILEGES IN SCHEMA dcsa_ebl_v1_0 GRANT ALL ON TABLES TO dcsa_db_owner;
+COMMIT;
