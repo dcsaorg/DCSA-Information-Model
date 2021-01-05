25f3b0c7e9bcfe9b06f603260345d38cae712094
Niels Thykier <nt@asseco.dk>
2021-01-05 12:50:35 +0100

    Add some of the foreign key constraints
    Signed-off-by: Niels Thykier <nt@asseco.dk>

--- datamodel/initdb.d/03_dcsa_im_v3_0.sql
+++ datamodel/initdb.d/03_dcsa_im_v3_0.sql
@@ -116,11 +116,9 @@ CREATE TABLE dcsa_ebl_v1_0.ebl_endorsement_chain (
 	title_holder uuid NOT NULL,
 	signature varchar(500) NOT NULL,
 	endorsement_datetime timestamp with time zone NOT NULL,
-	endorsee uuid NOT NULL
+	endorsee uuid NOT NULL,
+    CONSTRAINT "pk_ebl_endorsement_chain" PRIMARY KEY (transport_document_id,title_holder)
 );
-ALTER TABLE dcsa_ebl_v1_0.ebl_endorsement_chain ADD CONSTRAINT "pk_ebl_endorsement_chain"
-    PRIMARY KEY (transport_document_id,title_holder)
-;
 
 DROP TABLE IF EXISTS dcsa_ebl_v1_0.transport_document_carrier_clauses CASCADE;
 CREATE TABLE dcsa_ebl_v1_0.transport_document_carrier_clauses (
@@ -162,16 +160,27 @@ CREATE TABLE dcsa_ebl_v1_0.party (
 	nmfta_code varchar(4) NULL
 );
 
+DROP TABLE IF EXISTS dcsa_ebl_v1_0.party_function CASCADE;
+CREATE TABLE dcsa_ebl_v1_0.party_function (
+    party_function_code varchar(3) PRIMARY KEY,
+    party_function_name varchar(100) NOT NULL,
+    party_function_description varchar(250) NOT NULL
+);
+
 DROP TABLE IF EXISTS dcsa_ebl_v1_0.document_party CASCADE;
 CREATE TABLE dcsa_ebl_v1_0.document_party (
-    party_id uuid NOT NULL,
-    shipment_id uuid NULL,
-    shipping_instruction_id uuid NULL,
-    party_function varchar(3) NOT NULL,
+    party_id uuid NOT NULL REFERENCES dcsa_ebl_v1_0.party (id),
+    shipment_id uuid NULL REFERENCES dcsa_ebl_v1_0.shipment (id),
+    shipping_instruction_id uuid NULL REFERENCES dcsa_ebl_v1_0.shipping_instruction (id),
+    party_function varchar(3) NOT NULL REFERENCES dcsa_ebl_v1_0.party_function (party_function_code),
     displayed_address varchar(250) NULL,
     party_contact_details varchar(250) NULL,
     should_be_notified boolean NOT NULL
 );
+-- Supporting FK constraints
+CREATE INDEX ON dcsa_ebl_v1_0.document_party (party_id);
+CREATE INDEX ON dcsa_ebl_v1_0.document_party (shipment_id);
+CREATE INDEX ON dcsa_ebl_v1_0.document_party (shipping_instruction_id);
 
 DROP TABLE IF EXISTS dcsa_ebl_v1_0.carrier CASCADE;
 CREATE TABLE dcsa_ebl_v1_0.carrier (
@@ -179,13 +188,6 @@ CREATE TABLE dcsa_ebl_v1_0.carrier (
     carrier_name varchar(100),
     smdg_code varchar(3) NULL,
     nmfta_code varchar(4) NULL
-);
-
-DROP TABLE IF EXISTS dcsa_ebl_v1_0.party_function CASCADE;
-CREATE TABLE dcsa_ebl_v1_0.party_function (
-    party_function_code varchar(3) PRIMARY KEY,
-    party_function_name varchar(100) NOT NULL,
-    party_function_description varchar(250) NOT NULL
 );
 
 
@@ -211,15 +213,6 @@ CREATE TABLE dcsa_ebl_v1_0.charges (
 
 /* Equipment related Entities */
 
-
-DROP TABLE IF EXISTS dcsa_ebl_v1_0.equipment CASCADE;
-CREATE TABLE dcsa_ebl_v1_0.equipment (
-	equipment_reference varchar(15) PRIMARY KEY,	-- The unique identifier for the equipment, which should follow the BIC ISO Container Identification Number where possible. According to ISO 6346, a container identification code consists of a 4-letter prefix and a 7-digit number (composed of a 3-letter owner code, a category identifier, a serial number and a check-digit). If a container does not comply with ISO 6346, it is suggested to follow Recommendation #2 “Container with non-ISO identification” from SMDG.
-	iso_equipment_code char(4) NULL,	-- Unique code for the different equipment size/type used for transporting commodities. The code is a concatenation of ISO Equipment Size Code and ISO Equipment Type Code A and follows the ISO 6346 standard.
-	tare_weight real NULL,
-	weight_unit varchar(3) NULL
-);
-
 DROP TABLE IF EXISTS dcsa_ebl_v1_0.iso_equipment_code CASCADE;
 CREATE TABLE dcsa_ebl_v1_0.iso_equipment_code (
     iso_equipment_code varchar(4) PRIMARY KEY,
@@ -228,23 +221,45 @@ CREATE TABLE dcsa_ebl_v1_0.iso_equipment_code (
     iso_equipment_type_code_a varchar(2) NULL
 );
 
+DROP TABLE IF EXISTS dcsa_ebl_v1_0.equipment CASCADE;
+CREATE TABLE dcsa_ebl_v1_0.equipment (
+	equipment_reference varchar(15) PRIMARY KEY,	-- The unique identifier for the equipment, which should follow the BIC ISO Container Identification Number where possible. According to ISO 6346, a container identification code consists of a 4-letter prefix and a 7-digit number (composed of a 3-letter owner code, a category identifier, a serial number and a check-digit). If a container does not comply with ISO 6346, it is suggested to follow Recommendation #2 “Container with non-ISO identification” from SMDG.
+	-- Unique code for the different equipment size/type used for transporting commodities. The code is a concatenation of ISO Equipment Size Code and ISO Equipment Type Code A and follows the ISO 6346 standard.
+	-- FIXME: Add FK once we have iso_equipment_code data for our test
+	iso_equipment_code char(4) NULL /* REFERENCES dcsa_ebl_v1_0.iso_equipment_code (iso_equipment_code) */,
+	tare_weight real NULL,
+	weight_unit varchar(3) NULL
+);
+
+-- Supporting FK constraints
+CREATE INDEX ON dcsa_ebl_v1_0.equipment (iso_equipment_code);
 
 /* Stuffing related Entities */
 
 
+DROP TABLE IF EXISTS dcsa_ebl_v1_0.package_code CASCADE;
+CREATE TABLE dcsa_ebl_v1_0.package_code (
+	package_code varchar(3) PRIMARY KEY,
+	description varchar(50) NOT NULL
+);
+
 DROP TABLE IF EXISTS dcsa_ebl_v1_0.shipment_equipment CASCADE;
 CREATE TABLE dcsa_ebl_v1_0.shipment_equipment (
 	id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
-	shipment_id uuid NOT NULL,
-	equipment_reference varchar(15) NOT NULL,
+	shipment_id uuid NOT NULL REFERENCES dcsa_ebl_v1_0.shipment (id),
+	equipment_reference varchar(15) NOT NULL REFERENCES dcsa_ebl_v1_0.equipment (equipment_reference),
 	verified_gross_mass varchar(250) NULL,
 	cargo_gross_weight real NULL,
 	cargo_gross_weight_unit varchar(3) NULL
 );
 
+-- Supporting FK constraints
+CREATE INDEX ON dcsa_ebl_v1_0.shipment_equipment (equipment_reference);
+CREATE INDEX ON dcsa_ebl_v1_0.shipment_equipment (shipment_id);
+
 DROP TABLE IF EXISTS dcsa_ebl_v1_0.active_reefer_settings CASCADE;
 CREATE TABLE dcsa_ebl_v1_0.active_reefer_settings (
-	shipment_equipment_id uuid PRIMARY KEY,
+	shipment_equipment_id uuid PRIMARY KEY REFERENCES dcsa_ebl_v1_0.shipment_equipment (id),
 	temperature_min real NULL,
 	temperature_max real NULL,
 	temperature_unit varchar(3) NULL,
@@ -254,42 +269,43 @@ CREATE TABLE dcsa_ebl_v1_0.active_reefer_settings (
 	ventilation_max real NULL
 );
 
-DROP TABLE IF EXISTS dcsa_ebl_v1_0.cargo_item CASCADE;
-CREATE TABLE dcsa_ebl_v1_0.cargo_item (
-	id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
-	shipment_id uuid NOT NULL,
-	description_of_goods text NOT NULL,
-	hs_code varchar(10) NOT NULL,
-	weight real NULL,
-	volume real NULL,
-	weight_unit varchar(3) NULL,
-	volume_unit varchar(3) NULL,
-	number_of_packages integer NULL,
-	shipping_instruction_id uuid NULL,
-	package_code varchar(3) NULL,
-	shipment_equipment_id uuid NOT NULL
-);
-
-DROP TABLE IF EXISTS dcsa_ebl_v1_0.cargo_line_item CASCADE;
-CREATE TABLE dcsa_ebl_v1_0.cargo_line_item (
-	cargo_line_item_id text NOT NULL,
-	cargo_item_id uuid NOT NULL,
-	shipping_marks text NOT NULL
-);
-
 DROP TABLE IF EXISTS dcsa_ebl_v1_0.hs_code CASCADE;
 CREATE TABLE dcsa_ebl_v1_0.hs_code (
 	hs_code varchar(10) PRIMARY KEY,
 	code_description varchar(250) NOT NULL
 );
 
-DROP TABLE IF EXISTS dcsa_ebl_v1_0.seal CASCADE;
-CREATE TABLE dcsa_ebl_v1_0.seal (
-    id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
-	shipment_equipment_id uuid NOT NULL,
-	seal_number varchar(15) NOT NULL,
-	seal_source varchar(5) NOT NULL,
-	seal_type varchar(5) NOT NULL
+DROP TABLE IF EXISTS dcsa_ebl_v1_0.cargo_item CASCADE;
+CREATE TABLE dcsa_ebl_v1_0.cargo_item (
+	id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+	shipment_id uuid NOT NULL REFERENCES dcsa_ebl_v1_0.shipment (id),
+	description_of_goods text NOT NULL,
+	hs_code varchar(10) NOT NULL REFERENCES dcsa_ebl_v1_0.hs_code (hs_code),
+	weight real NULL,
+	volume real NULL,
+	weight_unit varchar(3) NULL,
+	volume_unit varchar(3) NULL,
+	number_of_packages integer NULL,
+	shipping_instruction_id uuid NULL REFERENCES dcsa_ebl_v1_0.shipping_instruction (id),
+	package_code varchar(3) NULL REFERENCES dcsa_ebl_v1_0.package_code (package_code),
+	shipment_equipment_id uuid NOT NULL REFERENCES dcsa_ebl_v1_0.shipment_equipment (id)
+);
+-- Supporting FK constraints
+CREATE INDEX ON dcsa_ebl_v1_0.cargo_item (shipment_id);
+CREATE INDEX ON dcsa_ebl_v1_0.cargo_item (hs_code);
+CREATE INDEX ON dcsa_ebl_v1_0.cargo_item (shipping_instruction_id);
+CREATE INDEX ON dcsa_ebl_v1_0.cargo_item (package_code);
+CREATE INDEX ON dcsa_ebl_v1_0.cargo_item (shipment_equipment_id);
+
+DROP TABLE IF EXISTS dcsa_ebl_v1_0.cargo_line_item CASCADE;
+CREATE TABLE dcsa_ebl_v1_0.cargo_line_item (
+	cargo_line_item_id text NOT NULL,
+	cargo_item_id uuid NOT NULL REFERENCES dcsa_ebl_v1_0.cargo_item (id),
+	shipping_marks text NOT NULL,
+	-- Choice of cargo_item_id as first member is deliberate as it enables the
+	-- underlying index to be used for FK checks as well (without a separate index
+	-- because Postgres currently always creates an INDEX for UNIQUE constraints)
+	UNIQUE (cargo_item_id, cargo_line_item_id)
 );
 
 DROP TABLE IF EXISTS dcsa_ebl_v1_0.seal_source CASCADE;
@@ -304,33 +320,21 @@ CREATE TABLE dcsa_ebl_v1_0.seal_type (
 	description varchar(50) NOT NULL
 );
 
-DROP TABLE IF EXISTS dcsa_ebl_v1_0.package_code CASCADE;
-CREATE TABLE dcsa_ebl_v1_0.package_code (
-	package_code varchar(3) PRIMARY KEY,
-	description varchar(50) NOT NULL
+DROP TABLE IF EXISTS dcsa_ebl_v1_0.seal CASCADE;
+CREATE TABLE dcsa_ebl_v1_0.seal (
+    id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+	shipment_equipment_id uuid NOT NULL REFERENCES dcsa_ebl_v1_0.shipment_equipment (id),
+	seal_number varchar(15) NOT NULL,
+	seal_source varchar(5) NOT NULL REFERENCES dcsa_ebl_v1_0.seal_source (seal_source_code),
+	seal_type varchar(5) NOT NULL REFERENCES dcsa_ebl_v1_0.seal_type (seal_type_code)
 );
+-- Supporting FK constraints
+CREATE INDEX ON dcsa_ebl_v1_0.seal (shipment_equipment_id);
+CREATE INDEX ON dcsa_ebl_v1_0.seal (seal_source);
+CREATE INDEX ON dcsa_ebl_v1_0.seal (seal_type);
 
 
 /* Location related Entities */
-
-
-DROP TABLE IF EXISTS dcsa_ebl_v1_0.location CASCADE;
-CREATE TABLE dcsa_ebl_v1_0.location (
-    id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
-    location_name varchar(100) NULL,
-	address varchar(250) NULL,
-	latitude varchar(10) NULL,
-	longitude varchar(11) NULL,
-	un_location_code char(5) NULL
-);
-
-DROP TABLE IF EXISTS dcsa_ebl_v1_0.shipment_location CASCADE;
-CREATE TABLE dcsa_ebl_v1_0.shipment_location (
-	shipment_id uuid NOT NULL,
-	location_id uuid NOT NULL,
-	location_type varchar(3) NOT NULL,
-	displayed_name varchar(250)
-);
 
 DROP TABLE IF EXISTS dcsa_ebl_v1_0.shipment_location_type CASCADE;
 CREATE TABLE dcsa_ebl_v1_0.shipment_location_type (
@@ -349,8 +353,37 @@ CREATE TABLE dcsa_ebl_v1_0.un_location (
 	un_location_code char(5) PRIMARY KEY,
 	un_location_name varchar(100) NULL,
 	location_code char(3) NULL,
-	country_code char(2) NULL
+	country_code char(2) NULL REFERENCES dcsa_ebl_v1_0.country (country_code)
 );
+-- Supporting FK constraints
+CREATE INDEX ON dcsa_ebl_v1_0.un_location (country_code);
+
+DROP TABLE IF EXISTS dcsa_ebl_v1_0.location CASCADE;
+CREATE TABLE dcsa_ebl_v1_0.location (
+    id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+    location_name varchar(100) NULL,
+	address varchar(250) NULL,
+	latitude varchar(10) NULL,
+	longitude varchar(11) NULL,
+	un_location_code char(5) NULL REFERENCES dcsa_ebl_v1_0.un_location (un_location_code)
+);
+-- Supporting FK constraints
+CREATE INDEX ON dcsa_ebl_v1_0.location (un_location_code);
+
+DROP TABLE IF EXISTS dcsa_ebl_v1_0.shipment_location CASCADE;
+CREATE TABLE dcsa_ebl_v1_0.shipment_location (
+	shipment_id uuid NOT NULL REFERENCES dcsa_ebl_v1_0.shipment (id),
+	location_id uuid NOT NULL REFERENCES dcsa_ebl_v1_0.location (id),
+	location_type varchar(3) NOT NULL REFERENCES dcsa_ebl_v1_0.shipment_location_type (location_type_code),
+	displayed_name varchar(250),
+	UNIQUE (location_id, location_type, shipment_id)
+);
+
+-- Supporting FK constraints
+-- Note the omission of INDEX for "location_id" is deliberate; we rely on the implicit INDEX from the
+-- UNIQUE constraint for that.
+CREATE INDEX ON dcsa_ebl_v1_0.shipment_location (location_type);
+CREATE INDEX ON dcsa_ebl_v1_0.shipment_location (shipment_id);
 
 DROP TABLE IF EXISTS dcsa_ebl_v1_0.facility CASCADE;
 CREATE TABLE dcsa_ebl_v1_0.facility (
@@ -475,44 +508,12 @@ CREATE TABLE dcsa_ebl_v1_0.commercial_voyage_transport_call (
 -- 	FOREIGN KEY (shipment_id) REFERENCES dcsa_ebl_v1_0.shipment (id) ON DELETE No Action ON UPDATE No Action
 -- ;
 --
--- ALTER TABLE dcsa_ebl_v1_0.cargo_item ADD CONSTRAINT "FK_Cargo Item_HS Code"
--- 	FOREIGN KEY (hs_code) REFERENCES dcsa_ebl_v1_0.hs_code (hs_code) ON DELETE No Action ON UPDATE No Action
--- ;
---
--- ALTER TABLE dcsa_ebl_v1_0.cargo_item ADD CONSTRAINT "FK_Cargo Item_Shipping Instruction"
--- 	FOREIGN KEY (shipping_instruction_number) REFERENCES dcsa_ebl_v1_0.shipping_instruction (shipping_instruction_number) ON DELETE No Action ON UPDATE No Action
--- ;
---
--- ALTER TABLE dcsa_ebl_v1_0.cargo_item ADD CONSTRAINT "FK_Commodity_Shipment"
--- 	FOREIGN KEY (shipment_id) REFERENCES dcsa_ebl_v1_0.shipment (id) ON DELETE No Action ON UPDATE No Action
--- ;
---
--- ALTER TABLE dcsa_ebl_v1_0.cargo_item_equipment ADD CONSTRAINT "FK_Shipment Equipment Commodity_Shipment Equipment"
--- 	FOREIGN KEY (shipment_equipment_id) REFERENCES dcsa_ebl_v1_0.shipment_equipment (id) ON DELETE No Action ON UPDATE No Action
--- ;
---
--- ALTER TABLE dcsa_ebl_v1_0.cargo_item_equipment ADD CONSTRAINT "FK_Shipment Equipment_Commodity"
--- 	FOREIGN KEY (cargo_item_id) REFERENCES dcsa_ebl_v1_0.cargo_item (id) ON DELETE No Action ON UPDATE No Action
--- ;
---
 -- ALTER TABLE dcsa_ebl_v1_0.charges ADD CONSTRAINT "FK_Charges_Location"
 -- 	FOREIGN KEY (freight_payable_at) REFERENCES dcsa_ebl_v1_0.location (id) ON DELETE No Action ON UPDATE No Action
 -- ;
 --
 -- ALTER TABLE dcsa_ebl_v1_0.charges ADD CONSTRAINT "FK_Charges_Shipment"
 -- 	FOREIGN KEY (shipment_id) REFERENCES dcsa_ebl_v1_0.shipment (id) ON DELETE No Action ON UPDATE No Action
--- ;
---
--- ALTER TABLE dcsa_ebl_v1_0.contact_details ADD CONSTRAINT "FK_Contact Details_Location"
--- 	FOREIGN KEY (location_id) REFERENCES dcsa_ebl_v1_0.location (id) ON DELETE No Action ON UPDATE No Action
--- ;
---
--- ALTER TABLE dcsa_ebl_v1_0.displayed_address ADD CONSTRAINT "FK_Printed Address_Party"
--- 	FOREIGN KEY (party_id) REFERENCES dcsa_ebl_v1_0.party (id) ON DELETE No Action ON UPDATE No Action
--- ;
---
--- ALTER TABLE dcsa_ebl_v1_0.displayed_address ADD CONSTRAINT "FK_Printed Address_Transport Document"
--- 	FOREIGN KEY (transport_document_id) REFERENCES dcsa_ebl_v1_0.transport_document (id) ON DELETE No Action ON UPDATE No Action
 -- ;
 --
 -- ALTER TABLE dcsa_ebl_v1_0.ebl_endorsement_chain ADD CONSTRAINT "FK_EBL Change Signing Parties_Party"
@@ -527,26 +528,6 @@ CREATE TABLE dcsa_ebl_v1_0.commercial_voyage_transport_call (
 -- 	FOREIGN KEY (transport_document_id) REFERENCES dcsa_ebl_v1_0.transport_document (id) ON DELETE No Action ON UPDATE No Action
 -- ;
 --
--- ALTER TABLE dcsa_ebl_v1_0.location ADD CONSTRAINT "FK_Location_UN Location"
--- 	FOREIGN KEY (un_location_code) REFERENCES dcsa_ebl_v1_0.un_location (un_location_code) ON DELETE No Action ON UPDATE No Action
--- ;
---
--- ALTER TABLE dcsa_ebl_v1_0.package_code ADD CONSTRAINT "FK_Standard Package Code_Shipment Item"
--- 	FOREIGN KEY (cargo_item_id) REFERENCES dcsa_ebl_v1_0.cargo_item (id) ON DELETE No Action ON UPDATE No Action
--- ;
---
--- ALTER TABLE dcsa_ebl_v1_0.party ADD CONSTRAINT "FK_Party_Location"
--- 	FOREIGN KEY (contact_details_id) REFERENCES dcsa_ebl_v1_0.contact_details (id) ON DELETE No Action ON UPDATE No Action
--- ;
---
--- ALTER TABLE dcsa_ebl_v1_0.party_location ADD CONSTRAINT "FK_Party Location_Location"
--- 	FOREIGN KEY (location_id) REFERENCES dcsa_ebl_v1_0.location (id) ON DELETE No Action ON UPDATE No Action
--- ;
---
--- ALTER TABLE dcsa_ebl_v1_0.party_location ADD CONSTRAINT "FK_Party Location_Party"
--- 	FOREIGN KEY (party_id) REFERENCES dcsa_ebl_v1_0.party (id) ON DELETE No Action ON UPDATE No Action
--- ;
---
 -- ALTER TABLE dcsa_ebl_v1_0.references ADD CONSTRAINT "FK_References_Reference Type"
 -- 	FOREIGN KEY (reference_type) REFERENCES dcsa_ebl_v1_0.reference_type (reference_type_code) ON DELETE No Action ON UPDATE No Action
 -- ;
@@ -555,48 +536,8 @@ CREATE TABLE dcsa_ebl_v1_0.commercial_voyage_transport_call (
 -- 	FOREIGN KEY (shipment_id) REFERENCES dcsa_ebl_v1_0.shipment (id) ON DELETE No Action ON UPDATE No Action
 -- ;
 --
--- ALTER TABLE dcsa_ebl_v1_0.seal ADD CONSTRAINT "FK_Seal_Seal Source"
--- 	FOREIGN KEY (seal_source_id) REFERENCES dcsa_ebl_v1_0.seal_source (id) ON DELETE No Action ON UPDATE No Action
--- ;
---
--- ALTER TABLE dcsa_ebl_v1_0.seal ADD CONSTRAINT "FK_Seal_Seal Type"
--- 	FOREIGN KEY (seal_type_id) REFERENCES dcsa_ebl_v1_0.seal_type (id) ON DELETE No Action ON UPDATE No Action
--- ;
---
--- ALTER TABLE dcsa_ebl_v1_0.seal ADD CONSTRAINT "FK_Seal_Shipment Equipment"
--- 	FOREIGN KEY (shipment_equipment_id) REFERENCES dcsa_ebl_v1_0.shipment_equipment (id) ON DELETE No Action ON UPDATE No Action
--- ;
---
 -- ALTER TABLE dcsa_ebl_v1_0.shipment ADD CONSTRAINT "FK_Shipment_Mode Of Transport"
 -- 	FOREIGN KEY (pre_carrier_mode_of_transport) REFERENCES dcsa_ebl_v1_0.mode_of_transport (mode_of_transport_code) ON DELETE No Action ON UPDATE No Action
--- ;
---
--- ALTER TABLE dcsa_ebl_v1_0.shipment_equipment ADD CONSTRAINT "FK_Shipment Equipment_Equipment"
--- 	FOREIGN KEY (equipment_reference) REFERENCES dcsa_ebl_v1_0.equipment (equipment_reference) ON DELETE No Action ON UPDATE No Action
--- ;
---
--- ALTER TABLE dcsa_ebl_v1_0.shipment_equipment ADD CONSTRAINT "FK_Shipment Equipment_Shipment"
--- 	FOREIGN KEY (shipment_id) REFERENCES dcsa_ebl_v1_0.shipment (id) ON DELETE No Action ON UPDATE No Action
--- ;
---
--- ALTER TABLE dcsa_ebl_v1_0.shipment_location ADD CONSTRAINT "FK_Shipment Location_Location"
--- 	FOREIGN KEY (location_id) REFERENCES dcsa_ebl_v1_0.location (id) ON DELETE No Action ON UPDATE No Action
--- ;
---
--- ALTER TABLE dcsa_ebl_v1_0.shipment_location ADD CONSTRAINT "FK_Shipment Location_Location Type"
--- 	FOREIGN KEY (location_type) REFERENCES dcsa_ebl_v1_0.shipment_location_type (location_type_code) ON DELETE No Action ON UPDATE No Action
--- ;
---
--- ALTER TABLE dcsa_ebl_v1_0.shipment_location ADD CONSTRAINT "FK_Shipment Location_Shipment"
--- 	FOREIGN KEY (shipment_id) REFERENCES dcsa_ebl_v1_0.shipment (id) ON DELETE No Action ON UPDATE No Action
--- ;
---
--- ALTER TABLE dcsa_ebl_v1_0.shipment_party ADD CONSTRAINT "FK_Notify Party_Party"
--- 	FOREIGN KEY (party_id) REFERENCES dcsa_ebl_v1_0.party (id) ON DELETE No Action ON UPDATE No Action
--- ;
---
--- ALTER TABLE dcsa_ebl_v1_0.shipment_party ADD CONSTRAINT "FK_Notify Party_Shipment"
--- 	FOREIGN KEY (shipment_id) REFERENCES dcsa_ebl_v1_0.shipment (id) ON DELETE No Action ON UPDATE No Action
 -- ;
 --
 -- ALTER TABLE dcsa_ebl_v1_0.shipment_transport ADD CONSTRAINT "FK_ShipmentTransportLeg_Shipment"
