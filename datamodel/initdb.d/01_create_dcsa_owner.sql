242efddaa8c41edbb386ee7028bd44b01ca51dce
Nicolas Gulibert <nicolas@ange.dk>
2021-01-27 10:06:10 +0100

    Update datamodel/initdb.d/01_create_dcsa_owner.sql
    Co-authored-by: Niels Thykier <nt@asseco.dk>

--- datamodel/initdb.d/01_create_dcsa_owner.sql
+++ datamodel/initdb.d/01_create_dcsa_owner.sql
@@ -1,4 +1,4 @@
 \set ON_ERROR_STOP true
 DROP DATABASE IF EXISTS dcsa_openapi;
-DROP USER dcsa_db_owner;
+DROP USER IF EXISTS dcsa_db_owner;
 CREATE USER dcsa_db_owner WITH PASSWORD '9c072fe8-c59c-11ea-b8d1-7b6577e9f3f5';
