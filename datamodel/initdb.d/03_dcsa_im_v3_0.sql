03cca4561bfa54ae40e2adb76868d51993d7cbc1
Henrik Helmø Larsen <hhl@asseco.dk>
2021-03-04 14:36:36 +0100

    Add DisplayCharges to SI

--- datamodel/initdb.d/03_dcsa_im_v3_0.sql
+++ datamodel/initdb.d/03_dcsa_im_v3_0.sql
@@ -139,6 +139,7 @@ CREATE TABLE dcsa_ebl_v1_0.shipping_instruction (
 	number_of_originals integer NULL,
 	freight_payable_at uuid NOT NULL,
 	is_electronic boolean NULL,
+	display_charges boolean NOT NULL,
 	callback_url text NOT NULL
 );
 
