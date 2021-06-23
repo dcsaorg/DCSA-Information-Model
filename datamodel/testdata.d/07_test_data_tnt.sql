141253e8741317c09a9ab2548e04150a8b7b12f8
Frazer Joe Dourado <frazer.dourado@dcsa.org>
2021-06-23 17:33:33 +0200

    updated 07 test data sql script with document type code

--- datamodel/testdata.d/07_test_data_tnt.sql
+++ datamodel/testdata.d/07_test_data_tnt.sql
@@ -93,7 +93,7 @@ INSERT INTO dcsa_im_v3_0.shipment_event (
     event_date_time,
     shipment_event_type_code,
     shipment_id,
-    shipment_information_type_code
+    document_type_code
 ) VALUES (
     uuid('784871e7-c9cd-4f59-8d88-2e033fa799a1'),
     'PLN',
@@ -111,7 +111,7 @@ INSERT INTO dcsa_im_v3_0.shipment_event (
     event_date_time,
     shipment_event_type_code,
     shipment_id,
-    shipment_information_type_code
+    document_type_code
 ) VALUES (
     uuid('e48f2bc0-c746-11ea-a3ff-db48243a89f4'),
     'PLN',
@@ -129,7 +129,7 @@ INSERT INTO dcsa_im_v3_0.shipment_event (
     event_date_time,
     shipment_event_type_code,
     shipment_id,
-    shipment_information_type_code
+    document_type_code
 ) VALUES (
     uuid('5e51e72c-d872-11ea-811c-0f8f10a32ea1'),
     'PLN',
