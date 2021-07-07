d3f6ae61d16129a27a02bc806644b6d9aaa06444
Frazer Joe Dourado <frazer.dourado@dcsa.org>
2021-07-07 12:35:45 +0200

    update reference impl script

--- datamodel/initdb.d/05_reference_implementation_support.sql
+++ datamodel/initdb.d/05_reference_implementation_support.sql
@@ -8,7 +8,6 @@ BEGIN;
 DROP VIEW IF EXISTS dcsa_im_v3_0.aggregated_events CASCADE;
 CREATE VIEW dcsa_im_v3_0.aggregated_events AS
  SELECT transport_event.event_id,
-    transport_event.event_type,
     transport_event.event_classifier_code,
     transport_event.transport_event_type_code,
     NULL::text AS document_type_code,
@@ -19,12 +18,10 @@ CREATE VIEW dcsa_im_v3_0.aggregated_events AS
     transport_event.delay_reason_code,
     transport_event.change_remark,
     NULL::text AS equipment_reference,
-    NULL::text AS empty_indicator_code,
-    NULL::UUID AS shipment_id
+    NULL::text AS empty_indicator_code
    FROM dcsa_im_v3_0.transport_event
 UNION
  SELECT shipment_event.event_id,
-    shipment_event.event_type,
     shipment_event.event_classifier_code,
     NULL::text AS transport_event_type_code,
     shipment_event.document_type_code,
@@ -35,12 +32,10 @@ UNION
     NULL::text AS delay_reason_code,
     NULL::text AS change_remark,
     NULL::text AS equipment_reference,
-    NULL::text AS empty_indicator_code,
-    shipment_event.shipment_id
+    NULL::text AS empty_indicator_code
    FROM dcsa_im_v3_0.shipment_event
 UNION
  SELECT equipment_event.event_id,
-    equipment_event.event_type,
     equipment_event.event_classifier_code,
     NULL::text AS transport_event_type_code,
     NULL::text AS document_type_code,
@@ -51,8 +46,7 @@ UNION
     NULL::text AS delay_reason_code,
     NULL::text AS change_remark,
     equipment_event.equipment_reference,
-    equipment_event.empty_indicator_code,
-    NULL::UUID AS shipment_id
+    equipment_event.empty_indicator_code
    FROM dcsa_im_v3_0.equipment_event;
 
 
