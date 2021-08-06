43717258e1bc72add7fb3c96896015cadccf50cb
kagemand-andersen <82385914+kagemand-andersen@users.noreply.github.com>
2021-08-06 15:23:38 +0200

    Update datamodel/testdata.d/08_03_test_data_ovs.sql
    Co-authored-by: Henrik Helmø Larsen <helmoe@gmail.com>

--- datamodel/testdata.d/08_03_test_data_ovs.sql
+++ datamodel/testdata.d/08_03_test_data_ovs.sql
@@ -221,7 +221,7 @@ INSERT INTO dcsa_im_v3_0.operations_event (
     'ANA',
     'KAT',
     'WSDP',
-    uuid('06aca2f6-f1d0-48f8-ba46-9a3480adfd23'),
+    '06aca2f6-f1d0-48f8-ba46-9a3480adfd23',
     'BRTH',
     uuid('be5bc290-7bac-48bb-a211-f3fa5a3ab3ae')
 );
