6e52ff6b4b4b0e88bb0787bc9388a7a72b17ad9f
Niels Thykier <nt@asseco.dk>
2021-10-21 16:16:16 +0200

    Add port call phase type code
    Signed-off-by: Niels Thykier <nt@asseco.dk>

--- datamodel/initdb.d/03_dcsa_im_v3_0.sql
+++ datamodel/initdb.d/03_dcsa_im_v3_0.sql
@@ -657,12 +657,20 @@ CREATE TABLE dcsa_im_v3_0.port_call_service_type (
     port_call_service_type_description varchar(250) NULL
 );
 
+DROP TABLE IF EXISTS dcsa_im_v3_0.port_call_phase_type CASCADE;
+CREATE TABLE dcsa_im_v3_0.port_call_phase_type (
+     port_call_phase_type_code varchar(4) NOT NULL PRIMARY KEY,
+     port_call_phase_type_name varchar(30) NOT NULL,
+     port_call_phase_type_description varchar(250) NULL
+);
+
 DROP TABLE IF EXISTS dcsa_im_v3_0.operations_event CASCADE;
 CREATE TABLE dcsa_im_v3_0.operations_event (
     operations_event_type_code varchar(4) NOT NULL REFERENCES dcsa_im_v3_0.operations_event_type(operations_event_type_code),
     transport_call_id varchar(100) NOT NULL REFERENCES dcsa_im_v3_0.transport_call(id),
     publisher varchar(100) NOT NULL,
     publisher_role varchar(3) NOT NULL,
+    port_call_phase_type_code varchar(4) NULL REFERENCES dcsa_im_v3_0.port_call_phase_type(port_call_phase_type_code),
     port_call_service_type_code varchar(4) NULL REFERENCES dcsa_im_v3_0.port_call_service_type(port_call_service_type_code),
     event_location varchar(100) NULL REFERENCES dcsa_im_v3_0.location (id),
     facility_type_code varchar(4) NULL REFERENCES dcsa_im_v3_0.facility_type(facility_type_code),
