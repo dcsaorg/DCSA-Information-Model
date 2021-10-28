3cea6c7205023874e073117960a8aba48738b5d3
Niels Thykier <nt@asseco.dk>
2021-10-28 13:13:41 +0200

    Correct test data
    Signed-off-by: Niels Thykier <nt@asseco.dk>

--- datamodel/testdata.d/08_02_test_data_tnt.sql
+++ datamodel/testdata.d/08_02_test_data_tnt.sql
@@ -5,6 +5,26 @@ BEGIN;
 
 SELECT 'Start: 08_02_test_data_tnt.sql...' as progress;
 
+INSERT INTO dcsa_im_v3_0.service (
+    carrier_id,
+    carrier_service_code,
+    carrier_service_name
+) VALUES (
+     (SELECT id FROM dcsa_im_v3_0.carrier WHERE smdg_code = 'MSK'),
+     'TNT1',
+     'Service name for TNT'
+);
+
+
+INSERT INTO dcsa_im_v3_0.voyage (
+    carrier_voyage_number,
+    service_id
+) VALUES (
+     'TNT1E',
+     (SELECT id FROM dcsa_im_v3_0.service WHERE carrier_service_code = 'TNT1' LIMIT 1)
+);
+
+
 INSERT INTO dcsa_im_v3_0.vessel (
     vessel_imo_number,
     vessel_name,
@@ -42,14 +62,18 @@ INSERT INTO dcsa_im_v3_0.transport_call (
     facility_id,
     facility_type_code,
     mode_of_transport,
-    vessel_id
+    vessel_id,
+    export_voyage_id,
+    import_voyage_id
 ) VALUES (
     '123e4567-e89b-12d3-a456-426614174000',
     1,
     (SELECT id FROM dcsa_im_v3_0.facility WHERE un_location_code = 'USNYC' AND facility_smdg_code = 'APMT'),
     'POTE',
     (SELECT mode_of_transport_code FROM dcsa_im_v3_0.mode_of_transport WHERE dcsa_transport_type = 'VESSEL'),
-    (SELECT id FROM dcsa_im_v3_0.vessel WHERE vessel_imo_number = '9321483')
+    (SELECT id FROM dcsa_im_v3_0.vessel WHERE vessel_imo_number = '9321483'),
+    (SELECT id FROM dcsa_im_v3_0.voyage WHERE carrier_voyage_number = 'TNT1E'),
+    (SELECT id FROM dcsa_im_v3_0.voyage WHERE carrier_voyage_number = 'TNT1E')
 );
 
 INSERT INTO dcsa_im_v3_0.transport (
