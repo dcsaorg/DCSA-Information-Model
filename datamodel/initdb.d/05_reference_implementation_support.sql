5628fb95062ad1b7e95fd940da9bfcc834ba3697
Niels Thykier <nt@asseco.dk>
2021-11-24 14:44:39 +0100

    Provide transport call view with timestamps
    Signed-off-by: Niels Thykier <nt@asseco.dk>

--- datamodel/initdb.d/05_reference_implementation_support.sql
+++ datamodel/initdb.d/05_reference_implementation_support.sql
@@ -353,4 +353,48 @@ CREATE OR REPLACE VIEW dcsa_im_v3_0.ui_timestamp_info AS
                             FROM dcsa_im_v3_0.pending_event_queue
         ) AS pending_event ON (pending_event.event_id = operations_event.event_id);
 
+CREATE INDEX ON dcsa_im_v3_0.operations_event (event_created_date_time);
+CREATE INDEX ON dcsa_im_v3_0.operations_event (transport_call_id);
+
+-- Only used by UI support to assist the UI
+DROP VIEW IF EXISTS dcsa_im_v3_0.transport_call_with_timestamps CASCADE;
+CREATE OR REPLACE VIEW dcsa_im_v3_0.transport_call_with_timestamps AS
+    SELECT transport_call.*,
+           latest_change.event_created_date_time AS latest_event_created_date_time,
+           latest_eta_berth.event_date_time AS eta_berth_date_time,
+           latest_atd_berth.event_date_time AS atd_berth_date_time
+           FROM dcsa_im_v3_0.transport_call
+      LEFT JOIN (SELECT MAX(event_created_date_time) AS event_created_date_time, transport_call_id
+                 FROM dcsa_im_v3_0.operations_event
+                 GROUP BY transport_call_id
+           ) AS latest_change ON (transport_call.id = latest_change.transport_call_id)
+      LEFT JOIN (
+               SELECT operations_event.event_date_time, operations_event.transport_call_id
+               FROM dcsa_im_v3_0.operations_event JOIN (
+                   SELECT MAX(event_created_date_time) AS event_created_date_time, transport_call_id
+                       FROM dcsa_im_v3_0.operations_event
+                       JOIN dcsa_im_v3_0.ops_event_timestamp_definition ON (operations_event.event_id = ops_event_timestamp_definition.event_id)
+                       JOIN dcsa_im_v3_0.timestamp_definition ON (timestamp_definition.id = ops_event_timestamp_definition.timestamp_definition)
+                       WHERE timestamp_definition.timestamp_type_name = 'ETA-Berth'
+                       GROUP BY transport_call_id
+                   ) AS latest_ts ON (operations_event.transport_call_id = latest_ts.transport_call_id AND operations_event.event_created_date_time = latest_ts.event_created_date_time)
+                   JOIN dcsa_im_v3_0.ops_event_timestamp_definition ON (operations_event.event_id = ops_event_timestamp_definition.event_id)
+                   JOIN dcsa_im_v3_0.timestamp_definition ON (timestamp_definition.id = ops_event_timestamp_definition.timestamp_definition)
+                   WHERE timestamp_definition.timestamp_type_name = 'ETA-Berth'
+          ) AS latest_eta_berth ON (transport_call.id = latest_eta_berth.transport_call_id)
+      LEFT JOIN (
+               SELECT operations_event.event_date_time, operations_event.transport_call_id
+               FROM dcsa_im_v3_0.operations_event JOIN (
+                   SELECT MAX(event_created_date_time) AS event_created_date_time, transport_call_id
+                       FROM dcsa_im_v3_0.operations_event
+                       JOIN dcsa_im_v3_0.ops_event_timestamp_definition ON (operations_event.event_id = ops_event_timestamp_definition.event_id)
+                       JOIN dcsa_im_v3_0.timestamp_definition ON (timestamp_definition.id = ops_event_timestamp_definition.timestamp_definition)
+                       WHERE timestamp_definition.timestamp_type_name = 'ATD-Berth'
+                       GROUP BY transport_call_id
+                   ) AS latest_ts ON (operations_event.transport_call_id = latest_ts.transport_call_id AND operations_event.event_created_date_time = latest_ts.event_created_date_time)
+                   JOIN dcsa_im_v3_0.ops_event_timestamp_definition ON (operations_event.event_id = ops_event_timestamp_definition.event_id)
+                   JOIN dcsa_im_v3_0.timestamp_definition ON (timestamp_definition.id = ops_event_timestamp_definition.timestamp_definition)
+                   WHERE timestamp_definition.timestamp_type_name = 'ATD-Berth'
+          ) AS latest_atd_berth ON (transport_call.id = latest_atd_berth.transport_call_id);
+
 COMMIT;
