77519c38cd6980598c6c923b5b33e398694ffa04
Henrik Helmø Larsen <helmoe@gmail.com>
2021-12-10 11:28:22 +0000

    DDT-660: Remove Booking from ShipmentTransport (#194)
    * Remove Booking from ShipmentTransport

--- datamodel/testdata.d/08_01_test_data_ebl.sql
+++ datamodel/testdata.d/08_01_test_data_ebl.sql
@@ -388,7 +388,6 @@ INSERT INTO dcsa_im_v3_0.transport (
 );
 
 INSERT INTO dcsa_im_v3_0.shipment_transport (
-    booking_id,
     shipment_id,
     transport_id,
     transport_plan_stage_sequence_number,
@@ -396,7 +395,6 @@ INSERT INTO dcsa_im_v3_0.shipment_transport (
     commercial_voyage_id,
     is_under_shippers_responsibility
 ) VALUES (
-    null,
     (SELECT id FROM dcsa_im_v3_0.shipment WHERE carrier_booking_reference = 'BR1239719871'),
     uuid('561a5606-402e-11eb-b19a-0f3aa4962e1f'),
     1,
@@ -404,7 +402,6 @@ INSERT INTO dcsa_im_v3_0.shipment_transport (
     null,
     false
 ), (
-    null,
     (SELECT id FROM dcsa_im_v3_0.shipment WHERE carrier_booking_reference = 'BR1239719871'),
     uuid('561a5606-402e-11eb-b19a-0f3aa4962e2f'),
     2,
@@ -412,7 +409,6 @@ INSERT INTO dcsa_im_v3_0.shipment_transport (
     null,
     false
 ), (
-    null,
     (SELECT id FROM dcsa_im_v3_0.shipment WHERE carrier_booking_reference = 'BR1239719871'),
     uuid('561a5606-402e-11eb-b19a-0f3aa4962e3f'),
     3,
