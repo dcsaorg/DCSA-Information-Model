3b2895fddebbba60c65c02b738f89482c10940ef
Henrik Helmø Larsen <helmoe@gmail.com>
2021-12-17 08:47:05 +0000

    Update datamodel/initdb.d/06_metadata.sql

--- datamodel/initdb.d/06_metadata.sql
+++ datamodel/initdb.d/06_metadata.sql
@@ -45,7 +45,7 @@ ALTER TABLE dcsa_im_v3_0.requested_equipment
 -- DateTime metadata
 
 -- Metadata for Booking table to avoid having to query shipmentEvent for 
--- modified date_time necessary for BookingResponseTO
+-- updated date_time necessary for BookingResponseTO
 
 ALTER TABLE dcsa_im_v3_0.booking
     ADD COLUMN IF NOT EXISTS updated_date_time timestamp with time zone NOT NULL;
