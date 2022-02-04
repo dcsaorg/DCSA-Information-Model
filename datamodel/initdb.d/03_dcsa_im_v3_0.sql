2e5cd441ebe3f4fab698688e1d935e5a92e80139
Henrik Helmø Larsen <helmoe@gmail.com>
2022-02-04 12:40:08 +0000

    DDT-798: Add 4x dispaled name fields to SI (#212)

--- datamodel/initdb.d/03_dcsa_im_v3_0.sql
+++ datamodel/initdb.d/03_dcsa_im_v3_0.sql
@@ -349,7 +349,11 @@ CREATE TABLE dcsa_im_v3_0.shipping_instruction (
     are_charges_displayed_on_originals boolean NOT NULL,
     are_charges_displayed_on_copies boolean NOT NULL,
     place_of_issue varchar(100) NULL REFERENCES dcsa_im_v3_0.location(id),
-    transport_document_type_code varchar(3) NULL REFERENCES dcsa_im_v3_0.transport_document_type(transport_document_type_code)
+    transport_document_type_code varchar(3) NULL REFERENCES dcsa_im_v3_0.transport_document_type(transport_document_type_code),
+    displayed_name_for_place_of_receipt varchar(250) NULL,
+    displayed_name_for_port_of_load varchar(250) NULL,
+    displayed_name_for_port_of_discharge varchar(250) NULL,
+    displayed_name_for_place_of_delivery varchar(250) NULL
 );
 
 DROP TABLE IF EXISTS dcsa_im_v3_0.transport_document CASCADE;
