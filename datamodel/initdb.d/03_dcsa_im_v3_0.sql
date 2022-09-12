136724084c4dea54430ddc74657e1db5217fdb98
Finn Nielsen <95635146+FinnAtAsseco@users.noreply.github.com>
2022-09-12 12:51:35 +0200

    Update datamodel/initdb.d/03_dcsa_im_v3_0.sql
    Co-authored-by: Henrik Helmø Larsen <helmoe@gmail.com>

--- datamodel/initdb.d/03_dcsa_im_v3_0.sql
+++ datamodel/initdb.d/03_dcsa_im_v3_0.sql
@@ -637,8 +637,8 @@ CREATE INDEX ON dcsa_im_v3_0.shipment_location (booking_id);
 DROP TABLE IF EXISTS dcsa_im_v3_0.port_call_status_code CASCADE;
 CREATE TABLE dcsa_im_v3_0.port_call_status_code (
     port_call_status_type varchar(4) NOT NULL PRIMARY KEY,
-    port_call_status_name varchar(30) NOT NULL,
-    port_call_status_description varchar(250) NOT NULL
+    port_call_status_type_name varchar(30) NOT NULL,
+    port_call_status_type_description varchar(250) NOT NULL
 );
 
 DROP TABLE IF EXISTS dcsa_im_v3_0.transport_call CASCADE;
