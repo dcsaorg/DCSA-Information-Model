f64ed4760409d47789b59a44e76fbf60baaf1c4d
Niels Thykier <nt@asseco.dk>
2022-11-17 12:42:34 +0100

    DDT-1308: Use ETA *or* PTA Berth for estimating arrival
    Signed-off-by: Niels Thykier <nt@asseco.dk>

--- datamodel/initdb.d/05_reference_implementation_support.sql
+++ datamodel/initdb.d/05_reference_implementation_support.sql
@@ -735,15 +735,15 @@ DROP VIEW IF EXISTS dcsa_im_v3_0.jit_port_visit_ui_context CASCADE;
 CREATE OR REPLACE VIEW dcsa_im_v3_0.jit_port_visit_ui_context AS
     SELECT jit_port_visit.port_visit_id,  -- port call visit
            latest_change.event_created_date_time AS latest_event_created_date_time,
-           latest_eta_berth.event_date_time AS eta_berth_date_time,
+           latest_eta_or_pta_berth.event_date_time AS best_berth_estimate_date_time,
            latest_atd_berth.event_date_time AS atd_berth_date_time,
            -- We use created for omits because it makes it easier for the UI to tell whether the OMIT is the latest
            -- timestamp (via tc.latest_event_created_date_time == tc.omit_created_date_time).
            -- The event_created_date_time timestamp is also useful for marking anything before that date time as
            -- obsolete.
            latest_omit.event_created_date_time AS omit_created_date_time,
-           latest_eta_berth.vessel_draft AS vessel_draft,
-           latest_eta_berth.miles_to_destination_port AS miles_to_destination_port
+           latest_eta_or_pta_berth.vessel_draft AS vessel_draft,
+           latest_eta_or_pta_berth.miles_to_destination_port AS miles_to_destination_port
            FROM dcsa_im_v3_0.jit_port_visit
       LEFT JOIN (SELECT MAX(event_created_date_time) AS event_created_date_time, transport_call_jit_port_visit.port_visit_id
                  FROM dcsa_im_v3_0.operations_event
@@ -760,13 +760,13 @@ CREATE OR REPLACE VIEW dcsa_im_v3_0.jit_port_visit_ui_context AS
                        JOIN dcsa_im_v3_0.transport_call_jit_port_visit ON operations_event.transport_call_id = transport_call_jit_port_visit.transport_call_id
                        JOIN dcsa_im_v3_0.ops_event_timestamp_definition ON (operations_event.event_id = ops_event_timestamp_definition.event_id)
                        JOIN dcsa_im_v3_0.timestamp_definition ON (timestamp_definition.timestamp_id = ops_event_timestamp_definition.timestamp_definition)
-                       WHERE timestamp_definition.timestamp_type_name IN ('ETA-Berth', 'ETA-Berth (<implicit>)')
+                       WHERE timestamp_definition.timestamp_type_name IN ('ETA-Berth', 'ETA-Berth (<implicit>)', 'PTA-Berth', 'PTA-Berth (<implicit>)')
                        GROUP BY port_visit_id
                    ) AS latest_ts ON (transport_call_jit_port_visit.port_visit_id = latest_ts.port_visit_id AND operations_event.event_created_date_time = latest_ts.event_created_date_time)
                    JOIN dcsa_im_v3_0.ops_event_timestamp_definition ON (operations_event.event_id = ops_event_timestamp_definition.event_id)
                    JOIN dcsa_im_v3_0.timestamp_definition ON (timestamp_definition.timestamp_id = ops_event_timestamp_definition.timestamp_definition)
-                   WHERE timestamp_definition.timestamp_type_name IN ('ETA-Berth', 'ETA-Berth (<implicit>)')
-          ) AS latest_eta_berth ON (jit_port_visit.port_visit_id = latest_eta_berth.port_visit_id)
+                   WHERE timestamp_definition.timestamp_type_name IN ('ETA-Berth', 'ETA-Berth (<implicit>)', 'PTA-Berth', 'PTA-Berth (<implicit>)')
+          ) AS latest_eta_or_pta_berth ON (jit_port_visit.port_visit_id = latest_eta_or_pta_berth.port_visit_id)
       LEFT JOIN (
                SELECT operations_event.event_date_time, transport_call_jit_port_visit.port_visit_id
                FROM dcsa_im_v3_0.operations_event
