287e650bfdfe6c8a0bc6c9c6d7ec2d8d14002a8c
gj0dcsa <135594855+gj0dcsa@users.noreply.github.com>
2026-06-08 09:39:07 +0200

    SD-2829 DO Excel sensitivity label

--- src/main/java/org/dcsa/standards/specifications/dataoverview/DataOverview.java
+++ src/main/java/org/dcsa/standards/specifications/dataoverview/DataOverview.java
@@ -3,6 +3,8 @@ package org.dcsa.standards.specifications.dataoverview;
 import io.swagger.v3.oas.models.media.Schema;
 import io.swagger.v3.oas.models.parameters.Parameter;
 import java.io.FileOutputStream;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -10,7 +12,7 @@ import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Stream;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ooxml.POIXMLProperties;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 @Slf4j
@@ -75,7 +77,8 @@ public class DataOverview {
 
   @SneakyThrows
   public void exportToExcelFile(String excelFilePath) {
-    try (Workbook workbook = new XSSFWorkbook()) {
+    try (XSSFWorkbook workbook = new XSSFWorkbook()) {
+      applySensitivityLabel(workbook);
       new LegendSheet(workbook, legendMetadata).addToWorkbook();
       AtomicLong nextId = new AtomicLong(0);
       Stream.of(
@@ -100,5 +103,21 @@ public class DataOverview {
             queryFiltersSheet)
         .filter(Objects::nonNull)
         .forEach(sheet -> sheet.exportToCsvFile(csvFilePattern));
+  }
+
+  private static void applySensitivityLabel(XSSFWorkbook wb) {
+    String labelId = "318d35dc-279b-461b-82fb-b370dac77106";
+    String prefix = "MSIP_Label_" + labelId + "_";
+
+    POIXMLProperties.CustomProperties custom = wb.getProperties().getCustomProperties();
+
+    custom.addProperty(prefix + "Enabled", "true");
+    custom.addProperty(prefix + "SetDate", Instant.now().truncatedTo(ChronoUnit.SECONDS).toString());
+    custom.addProperty(prefix + "Method", "Privileged");
+    custom.addProperty(prefix + "Name", "Project");
+    custom.addProperty(prefix + "SiteId", "e5d7661a-17a9-4d48-a5fe-302735ec282c");
+    custom.addProperty(prefix + "ActionId", "973f9577-29d6-456c-8c9b-43c06aab57e3");
+    custom.addProperty(prefix + "ContentBits", "0");
+    custom.addProperty(prefix + "Tag", "10, 0, 1, 1");
   }
 }
